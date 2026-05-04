cdb39b64cfddd9c51317f3df6180c07eaaa92874
Mauricio Tognocchi <160805898+MauriTognocchi@users.noreply.github.com>
2026-05-04 16:01:18 -0300

    feat: Implementar DiagnosisDTOs con validaciones
    Se agregan DiagnosisRequestDTO y DiagnosisReponseDTO incluyendo validaciones con Jakarta Validation

--- src/main/java/com/mokah/veterinary/features/diagnosis/dto/DiagnosisResponseDTO.java
+++ src/main/java/com/mokah/veterinary/features/diagnosis/dto/DiagnosisResponseDTO.java
@@ -1,4 +1,18 @@
 package com.mokah.veterinary.features.diagnosis.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DiagnosisResponseDTO {
+
+    private Long id;
+    private String descripcion;
+    private LocalDateTime fechaDiagnostico;
+    private String observaciones;
 }
