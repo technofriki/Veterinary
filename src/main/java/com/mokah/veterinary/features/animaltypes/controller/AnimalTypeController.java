96ca342ef4dbf3f3527159f13e6de1c1ea04212b
technofriki <jack_mdq97@hotmail.com>
2026-05-05 16:41:19 -0300

    Controller AnimalType

---
+++ src/main/java/com/mokah/veterinary/features/animaltypes/controller/AnimalTypeController.java
@@ -0,0 +1,58 @@
+package com.mokah.veterinary.features.animaltypes.controller;
+
+import com.mokah.veterinary.features.animaltypes.dto.AnimalTypeRequest;
+import com.mokah.veterinary.features.animaltypes.dto.AnimalTypeResponse;
+import com.mokah.veterinary.features.animaltypes.service.AnimalTypeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/animal-types") ///checkear que sea singular o plural
+public class AnimalTypeController {
+
+    private final AnimalTypeService animalTypeService;
+
+    public AnimalTypeController(AnimalTypeService animalTypeService) {
+        this.animalTypeService = animalTypeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AnimalTypeResponse>> getAllAnimalTypes() {
+        List<AnimalTypeResponse> responseList = animalTypeService.findAll();
+        return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AnimalTypeResponse> getAnimalTypeById (@PathVariable Long id){
+        AnimalTypeResponse response = animalTypeService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<AnimalTypeResponse> getAnimalTypeByName (@PathVariable String name){
+        AnimalTypeResponse response = animalTypeService.findByName(name);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<AnimalTypeResponse> createAnimalType (@RequestBody AnimalTypeRequest request){
+        AnimalTypeResponse created = animalTypeService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping
+    public ResponseEntity<AnimalTypeResponse> updateAnimalType (@PathVariable Long id, @RequestBody AnimalTypeRequest request){
+        AnimalTypeResponse updated = animalTypeService.update(id, request);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<AnimalTypeResponse> deleteAnimalType (@PathVariable Long id){
+        animalTypeService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
+
