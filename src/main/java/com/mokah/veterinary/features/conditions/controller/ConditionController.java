be5804e4d58a32edd0e404acec9c35970c5776ef
Mauricio Tognocchi <160805898+MauriTognocchi@users.noreply.github.com>
2026-05-10 15:13:21 -0300

    refactor: RequestMapping plural

--- src/main/java/com/mokah/veterinary/features/conditions/controller/ConditionController.java
+++ src/main/java/com/mokah/veterinary/features/conditions/controller/ConditionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/condition")
+@RequestMapping("/api/conditions")
 public class ConditionController {
 
     private final ConditionService conditionService;
