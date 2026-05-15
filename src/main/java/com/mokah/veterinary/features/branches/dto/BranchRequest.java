513f779e81e34f706367223e3d95fc986e0420e4
Mauricio Tognocchi <160805898+MauriTognocchi@users.noreply.github.com>
2026-05-14 23:51:54 -0300

    feat: crear DTOs para Branch
    Crea BranchRequest con validaciones y BranchResponse

---
+++ src/main/java/com/mokah/veterinary/features/branches/dto/BranchRequest.java
@@ -0,0 +1,32 @@
+package com.mokah.veterinary.features.branches.dto;
+
+import com.mokah.veterinary.features.adresses.dto.AddressRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BranchRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank(message = "Phone is required")
+    @Size(max = 20)
+    private String phone;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid format email")
+    @Size(max = 50)
+    private String email;
+
+    @Valid // que haria valid aca? usaria las validaciones del AddressRequest?
+    private AddressRequest address;
+}
