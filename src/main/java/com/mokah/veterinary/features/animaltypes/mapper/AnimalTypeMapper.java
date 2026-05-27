3bc9d6b50f37b6ec7cbd8d487c96f4c6c31e0224
technofriki <jack_mdq97@hotmail.com>
2026-05-27 15:14:46 -0300

    Update AnimalTypeMapper
    Habia algo escrito de mas al ser la primera feature. ya esta funcionando igual

--- src/main/java/com/mokah/veterinary/features/animaltypes/mapper/AnimalTypeMapper.java
+++ src/main/java/com/mokah/veterinary/features/animaltypes/mapper/AnimalTypeMapper.java
@@ -10,10 +10,9 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface AnimalTypeMapper {
 
-   @Mapping(target = "id", ignore = true)
-    @Mapping(source = "name", target = "animalType")
+
     AnimalTypeEntity toEntity(AnimalTypeRequest request);
 
-   @Mapping(source = "animalType", target = "name")
+
    AnimalTypeResponse toResponse(AnimalTypeEntity entity);
 }
