b62b1aed6a54e9c15b376363c6764aeb41b44e3f
technofriki <jack_mdq97@hotmail.com>
2026-06-04 20:42:43 -0300

    Feature Visit
    Visit Request created

---
+++ src/main/java/com/mokah/veterinary/features/visits/dto/VisitRequest.java
@@ -0,0 +1,15 @@
+package com.mokah.veterinary.features.visits.dto;
+
+import com.mokah.veterinary.features.appointments.model.Appointment;
+import com.mokah.veterinary.features.veterinarians.model.Veterinarian;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record VisitRequest(
+        @NotBlank String observations,
+        @NotNull @Positive Long appointmentId,
+        @NotNull @Positive Veterinarian veterinarian
+) {
+}
