d1928e338d3443bbafca4102ca247dbf6397e5ac
technofriki <jack_mdq97@hotmail.com>
2026-06-04 20:41:44 -0300

    Feature Visit
    Entity made

---
+++ src/main/java/com/mokah/veterinary/features/visits/entity/VisitEntity.java
@@ -0,0 +1,31 @@
+package com.mokah.veterinary.features.visits.entity;
+
+import com.mokah.veterinary.features.appointments.model.Appointment;
+import com.mokah.veterinary.features.veterinarians.model.Veterinarian;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "visits")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class VisitEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(nullable = false)
+    private String obervations;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appoinment_id", nullable = false)
+    private Appointment appointment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "veterinarian_id", nullable = false)
+    private Veterinarian  veterinarian;
+}
