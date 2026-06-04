49fa3552d5f02469442d8e50d9f7afa351f0c4d3
Mauricio Tognocchi <160805898+MauriTognocchi@users.noreply.github.com>
2026-06-04 13:27:45 -0300

    feat: implementar Service con PredicateSpecification

--- src/main/java/com/mokah/veterinary/features/appointments/service/AppointmentService.java
+++ src/main/java/com/mokah/veterinary/features/appointments/service/AppointmentService.java
@@ -1,4 +1,28 @@
 package com.mokah.veterinary.features.appointments.service;
 
+import com.mokah.veterinary.features.appointments.dto.AppointmentCreateDTO;
+import com.mokah.veterinary.features.appointments.dto.AppointmentResponse;
+import com.mokah.veterinary.features.appointments.dto.AppointmentUpdateDTO;
+import com.mokah.veterinary.features.appointments.model.AppointmentStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AppointmentService {
+
+    AppointmentResponse create(AppointmentCreateDTO dto);
+
+    List<AppointmentResponse> findAll(
+            LocalDateTime date,
+            String reason,
+            AppointmentStatus status,
+            Long petId,
+            Long veterinarianId
+    );
+
+    AppointmentResponse findById(Long id);
+
+    AppointmentResponse update(Long id, AppointmentUpdateDTO dto);
+
+    void delete(Long id);
 }
