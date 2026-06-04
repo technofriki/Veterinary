6bd7f158915465282767622eaa27f1b6098efbdd
Mauricio Tognocchi <160805898+MauriTognocchi@users.noreply.github.com>
2026-06-04 13:26:50 -0300

    feat: implementar DTOs para Appointment

--- src/main/java/com/mokah/veterinary/features/appointments/dto/AppointmentResponse.java
+++ src/main/java/com/mokah/veterinary/features/appointments/dto/AppointmentResponse.java
@@ -1,4 +1,14 @@
 package com.mokah.veterinary.features.appointments.dto;
 
-public class AppointmentResponse {
-}
+import com.mokah.veterinary.features.pets.dto.PetResponse;
+import com.mokah.veterinary.features.veterinarians.dto.VeterinarianResponse;
+
+import java.time.LocalDateTime;
+
+public record AppointmentResponse(
+        LocalDateTime appointmentDate,
+        String reason,
+        String status,
+        PetResponse pet,
+        VeterinarianResponse veterinarian
+) {}
