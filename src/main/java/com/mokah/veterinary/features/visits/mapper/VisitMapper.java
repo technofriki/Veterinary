b9ebcb0065c55f1a5270b7990da5008b836e80f4
technofriki <jack_mdq97@hotmail.com>
2026-06-06 02:41:25 -0300

    Visit Mapper, Controller, Service

--- src/main/java/com/mokah/veterinary/features/visits/mapper/VisitMapper.java
+++ src/main/java/com/mokah/veterinary/features/visits/mapper/VisitMapper.java
@@ -1,18 +1,28 @@
 package com.mokah.veterinary.features.visits.mapper;
 
+import com.mokah.veterinary.features.appointments.dto.AppointmentUpdateDTO;
 import com.mokah.veterinary.features.appointments.mapper.AppointmentMapper;
 import com.mokah.veterinary.features.veterinarians.mapper.VeterinarianMapper;
 import com.mokah.veterinary.features.visits.dto.VisitRequest;
 import com.mokah.veterinary.features.visits.dto.VisitResponse;
 import com.mokah.veterinary.features.visits.entity.VisitEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {VeterinarianMapper.class, AppointmentMapper.class})
 public interface VisitMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "veterinarian", ignore = true)
+    @Mapping(target = "appointment", ignore = true)
     VisitEntity toEntity (VisitRequest request);
     VisitResponse toResponse (VisitEntity entity);
     List<VisitResponse> toResponseList (List<VisitEntity> entities);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "veterinarian", ignore = true)
+    @Mapping(target = "appointment", ignore = true)
+    void update(@MappingTarget VisitEntity entity, VisitRequest request);
 }
