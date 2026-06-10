12e952c15c3c63f9730e2737b6815b7af0f762e2
technofriki <jack_mdq97@hotmail.com>
2026-06-10 19:55:27 -0300

    Update branch
    Updated with Opening and Closing time

--- src/main/java/com/mokah/veterinary/features/branches/model/Branch.java
+++ src/main/java/com/mokah/veterinary/features/branches/model/Branch.java
@@ -4,6 +4,7 @@ import com.mokah.veterinary.features.adresses.model.Address;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Entity
@@ -22,9 +23,18 @@ public class Branch {
     @Column(name = "external_id", nullable = false, unique = true, updatable = false)
     private UUID externalId;
 
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
     private String phone;
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(name = "opening_time", nullable = false)
+    private LocalTime openingTime;
+
+    @Column(name = "closing_time", nullable = false)
+    private LocalTime closingTime;
 
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "address_id")
