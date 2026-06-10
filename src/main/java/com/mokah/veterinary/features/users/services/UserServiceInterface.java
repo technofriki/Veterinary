8fa94d6f4194ce2ab73dc95924f2bdbbfbce6512
technofriki <jack_mdq97@hotmail.com>
2026-06-10 19:58:48 -0300

    Update UserService
    UserService includes exceptions

--- src/main/java/com/mokah/veterinary/features/users/services/UserServiceInterface.java
+++ src/main/java/com/mokah/veterinary/features/users/services/UserServiceInterface.java
@@ -1,11 +1,14 @@
 package com.mokah.veterinary.features.users.services;
 
 import com.mokah.veterinary.features.users.dto.UserResponse;
+import com.mokah.veterinary.features.users.model.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserServiceInterface {
+
+    User entityByExternalId(UUID externalId);
 
     List<UserResponse> findAll();
 
