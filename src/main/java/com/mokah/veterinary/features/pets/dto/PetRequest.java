587fbdc1441d8b5b55856b2f21b002502808b352
Mauricio Tognocchi <160805898+MauriTognocchi@users.noreply.github.com>
2026-06-17 17:47:12 -0300

    Update PetRequest.java

--- src/main/java/com/mokah/veterinary/features/pets/dto/PetRequest.java
+++ src/main/java/com/mokah/veterinary/features/pets/dto/PetRequest.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 public record PetRequest(
         @NotBlank String name,
